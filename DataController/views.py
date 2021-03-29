7f2f7c95366be9cc4d4fccc3f626bbdea8db1254
Sohanuzzaman Soad <mdsoad@gmail.com>
2021-03-29 13:09:53 +0600

    Implements Views Function For HeartRate and Blood Pressure

--- DataController/views.py
+++ DataController/views.py
@@ -1,3 +1,87 @@
+import json
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from DataController.models import HeartRate, BloodPressure
+from DeviceController.models import Device
+
+
+def HeartRateData(request, ip):
+    print(ip)
+    devices = Device.objects.get(IP_ADDRESS=ip)
+    heart_rate_labels = []
+    heart_rate_data = []
+    heart_data = HeartRate.objects.filter(DEVICE=devices)
+    i = 0
+    for ht in heart_data:
+        heart_rate_labels.append(str(ht.DATETIME.time()))
+        heart_rate_data.append(ht.VALUE)
+        i += 1
+        if i > 9:
+            break
+
+    heart_rate = {
+        "labels": heart_rate_labels,
+        "datasets": [
+            {
+                "data": heart_rate_data,
+                "backgroundColor": "rgba(202, 201, 197, 0.5)",
+                "borderColor": "rgba(202, 201, 197, 1)",
+                "pointBackgroundColor": "rgba(35, 78, 123, 0.69)",
+                "pointBorderColor": "#fff",
+                "label": "Heart Rate Data",
+                "name": "Heart Rate Data"
+            }
+        ]
+    }
+    dic = {
+        "heart_rate": heart_rate,
+    }
+    # data = serialize("json", dic.all())
+    # data1={}
+    # for d in data:
+    #     print(type(d))
+    # data1[d['pk']] = d['fields']
+    print()
+    return HttpResponse(json.dumps(dic), content_type="application/json")
+
+
+def BloodPressureData(request, ip):
+    devices = Device.objects.get(IP_ADDRESS=ip)
+    blood_pressure_labels = []
+    blood_pressure_data = []
+    heart_data = BloodPressure.objects.filter(DEVICE=devices)
+    i = 0
+    for ht in heart_data:
+        blood_pressure_labels.append(str(ht.DATETIME.time()))
+        blood_pressure_data.append(ht.VALUE)
+        i += 1
+        if i > 9:
+            break
+
+    blood_pressure = {
+        "labels": blood_pressure_labels,
+        "datasets": [
+            {
+                "data": blood_pressure_data,
+                "backgroundColor": "rgba(202, 201, 197, 0.5)",
+                "borderColor": "rgba(202, 201, 197, 1)",
+                "pointBackgroundColor": "rgba(35, 78, 123, 0.69)",
+                "pointBorderColor": "#fff",
+                "label": "Blood Pressure Data",
+                "name": "Blood Pressure Data"
+            }
+        ]
+    }
+    dic = {
+        "blood_pressure": blood_pressure,
+    }
+    # data = serialize("json", dic.all())
+    # data1={}
+    # for d in data:
+    #     print(type(d))
+    # data1[d['pk']] = d['fields']
+    print()
+    return HttpResponse(json.dumps(dic), content_type="application/json")
--- PatientMonitoringSystem/urls.py
+++ PatientMonitoringSystem/urls.py
@@ -1,21 +1,13 @@
-"""PatientMonitoringSystem URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from GraphController.views import line_chart, line_chart_json, dataView
+from DataController.views import HeartRateData, BloodPressureData
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chart', line_chart, name='line_chart'),
+    path('chartJSON', line_chart_json, name='line_chart_json'),
+    path('heart_data/<str:ip>', HeartRateData),
+    path('blood_data/<str:ip>', BloodPressureData),
+    path('view/<str:ip>', dataView)
 ]
